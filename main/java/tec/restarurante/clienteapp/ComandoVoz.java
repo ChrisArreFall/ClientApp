e313eeb10431396392f44607e906885a21d0ced2
raxzers <gacova95@gmail.com>
2016-10-24 18:52:19 -0600

    corregido la intreaccion en voz
    se puede utilzar en el boton de voz en platillo (no testeado)

---
+++ main/java/tec/restarurante/clienteapp/ComandoVoz.java
@@ -0,0 +1,66 @@
+package tec.restarurante.clienteapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import java.util.ArrayList;
+import java.util.Locale;
+import android.app.Activity;
+import android.content.ActivityNotFoundException;
+import android.content.Intent;
+import android.os.Bundle;
+import android.speech.RecognizerIntent;
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class ComandoVoz extends AppCompatActivity {
+    private final int SPEECH_RECOGNITION_CODE = 1;
+    private TextView txtOutput;
+    private ImageButton btnMicrophone;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_comando_voz);
+        txtOutput = (TextView) findViewById(R.id.txt_output);
+        btnMicrophone = (ImageButton) findViewById(R.id.btn_mic);
+        btnMicrophone.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startSpeechToText();
+            }
+        });
+    }
+
+    private void startSpeechToText() {
+        Intent intent = new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
+        intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE, Locale.getDefault());
+        intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL,
+                RecognizerIntent.LANGUAGE_MODEL_FREE_FORM);
+        intent.putExtra(RecognizerIntent.EXTRA_PROMPT,
+                "Speak something...");
+        try {
+            startActivityForResult(intent, SPEECH_RECOGNITION_CODE);
+        } catch (ActivityNotFoundException a) {
+            Toast.makeText(getApplicationContext(),
+                    "Sorry! Speech recognition is not supported in this device.",
+                    Toast.LENGTH_SHORT).show();
+        }
+    }
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        switch (requestCode) {
+            case SPEECH_RECOGNITION_CODE: {
+                if (resultCode == RESULT_OK && null != data) {
+                    ArrayList<String> result = data
+                            .getStringArrayListExtra(RecognizerIntent.EXTRA_RESULTS);
+                    String text = result.get(0);
+                    txtOutput.setText(text);
+                }
+                break;
+            }
+        }
+    }
+
+}
--- main/java/tec/restarurante/clienteapp/platillo.java
+++ main/java/tec/restarurante/clienteapp/platillo.java
@@ -3,36 +3,48 @@ package tec.restarurante.clienteapp;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
-import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
-import android.view.View;
 import android.support.design.widget.NavigationView;
+
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
-
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.TextView;
+import java.util.ArrayList;
+import java.util.Locale;
+import android.content.ActivityNotFoundException;
+import android.speech.RecognizerIntent;
+import android.widget.Toast;
+
+
 
 public class platillo extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener{
+
+    private final int SPEECH_RECOGNITION_CODE = 1;
+    private TextView txtOutput;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_platillo);
+        txtOutput = (TextView) findViewById(R.id.txt_output);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+            public void onClick(View v) {
+                startSpeechToText();
             }
-        });
+        }   );
+
+
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
                 this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
@@ -42,6 +54,37 @@ public class platillo extends AppCompatActivity
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
     }
+    private void startSpeechToText() {
+        Intent intent = new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
+        intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE, Locale.getDefault());
+        intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL,
+                RecognizerIntent.LANGUAGE_MODEL_FREE_FORM);
+        intent.putExtra(RecognizerIntent.EXTRA_PROMPT,
+                "Speak something...");
+        try {
+            startActivityForResult(intent, SPEECH_RECOGNITION_CODE);
+        } catch (ActivityNotFoundException a) {
+            Toast.makeText(getApplicationContext(),
+                    "Sorry! Speech recognition is not supported in this device.",
+                    Toast.LENGTH_SHORT).show();
+        }
+    }
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        switch (requestCode) {
+            case SPEECH_RECOGNITION_CODE: {
+                if (resultCode == RESULT_OK && null != data) {
+                    ArrayList<String> result = data
+                            .getStringArrayListExtra(RecognizerIntent.EXTRA_RESULTS);
+                    String text = result.get(0);
+                    txtOutput.setText(text);
+                }
+                break;
+            }
+        }
+    }
+
+
 
     @Override
     public void onBackPressed() {
@@ -69,7 +112,7 @@ public class platillo extends AppCompatActivity
 
         //noinspection SimplifiableIfStatement
         if (id == R.id.action_settings) {
-
+            startActivity(new Intent(this,ComandoVoz.class));
             return true;
         }
 
@@ -87,7 +130,7 @@ public class platillo extends AppCompatActivity
             startActivity(new Intent(this,platillo.class));
         } else if (id == R.id.nav_currentOrder) {
             this.finish();
-            startActivity(new Intent(this,OrdenActual.class));            ;
+            startActivity(new Intent(this,OrdenActual.class));
         } else if (id == R.id.nav_checkOut) {
             this.finish();
             startActivity(new Intent(this,CheckOut.class));
